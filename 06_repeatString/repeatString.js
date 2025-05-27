16891394c554b092ca364572b67cb387ab33ddc8
dantedanette0 <carminedante@yahoo.com>
2025-05-27 17:42:16 -0400

    1 to 7 done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(s , n) {
 
+    word = ""
+
+    if (n < 0){
+        return "ERROR"
+    }
+
+    for(let i = 0; i < n;i++){
+        word += s
+    }
+    return word
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+let arr = s.split('');     
+let left = 0;
+let right = arr.length - 1;
+while (left < right) {
+  const tmp     = arr[left];
+  arr[left]  = arr[right];
+  arr[right] = tmp;
+  left++;
+  right--;
+}
+return arr.join('');  
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
